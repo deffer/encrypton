e297494bbfb8cca9de2087181d71584e0ca74fee
deffer <irina.benediktovich@gmail.com>
2015-05-20 16:58:36 +1200

    Try to use timeout to replay the process

--- encrypton.js
+++ encrypton.js
@@ -9,20 +9,44 @@ function linebrk(s,n) {
 }
 
 var app = angular.module("myEncrApp", []); 
-var modpStr = "d61c97da3b0db88f726511e346bce8ce302ba363ca38caef4d68bf72efe7dad7b8102fa38190ffb42148617421fadaaa02a43d1394638cf61f214af91cd57579d3aefd4a728fb0423c2b7d31f7672816993f95ca5490316c9a9c8dfc4bccb5d2fcc97a109e57d7005457a8f4a4ddda460b8ec4a7ce0ccc778c4806a75392b838a6b4ac3e675665fe20859399749713ace1ce74e8f8ce714ec8b10c76d68a22662e4193f11e70061ef6103b6050a73dab2aed811cf33e51fd04a38f5b8b6ee075"
-var modsStr = "b59944da36a9ec70ac426cbcefb718a41d9a686ee5088ab0e8422bf58a4d9da3aae211ec28866babda6ff13a6c2ef84928ea464b956d6e09548ee014ba9c0e8dd3ba5a8a48bf5f020fa58af897a2cfa2888c5a1ebca481935e14d4528a6fbb08bf24e985edf4845adfb307301045790f1d1be0c1326b692b453fa94c1033e2e881e2538fa729590fbe72bfcfe7364c4ca9e3de403e597fce6d89d0a7890eec5d32d854795f0ca9a3bd56c5395affa5791544c6f81026e08d7b668f4548d7972f"
+var pStr = "";
+var rnd1Str =  "d61c97da3b0db88f726511e346bce8ce302ba363ca38caef4d68bf72efe7dad7b8102fa38190ffb42148617421fadaaa02a43d1394638cf61f214af91cd57579d3aefd4a728fb0423c2b7d31f7672816993f95ca5490316c9a9c8dfc4bccb5d2fcc97a109e57d7005457a8f4a4ddda460b8ec4a7ce0ccc778c4806a75392b838a6b4ac3e675665fe20859399749713ace1ce74e8f8ce714ec8b10c76d68a22662e4193f11e70061ef6103b6050a73dab2aed811cf33e51fd04a38f5b8b6ee075"
+var rnd2Str = "b59944da36a9ec70ac426cbcefb718a41d9a686ee5088ab0e8422bf58a4d9da3aae211ec28866babda6ff13a6c2ef84928ea464b956d6e09548ee014ba9c0e8dd3ba5a8a48bf5f020fa58af897a2cfa2888c5a1ebca481935e14d4528a6fbb08bf24e985edf4845adfb307301045790f1d1be0c1326b692b453fa94c1033e2e881e2538fa729590fbe72bfcfe7364c4ca9e3de403e597fce6d89d0a7890eec5d32d854795f0ca9a3bd56c5395affa5791544c6f81026e08d7b668f4548d7972f"
 
-app.controller("myEncrCtrl", function($scope) {
+app.controller("myEncrCtrl", function($scope, $timeout) {
     $scope.message = "";
 	$scope.lines = [];
     $scope.start  = function() {
-		var nextRandom = new BigInteger(1536,1,new SecureRandom());
-		$scope.lines.push({message:
-			linebrk(nextRandom.toString(16), 64)
-			//linebrk(hex2b64(nextRandom.toString(16)), 64)
-			}
-		);
+		var a2, a3, g2a, str;
+		
+		var pHex = b64tohex(pStr);
+		var p = new BigInteger(pHex, 16);
+		var g1 = new BigInteger("02", 16);
+		$scope.lines.push({message: "Given p = 1536 bits prime and g1 = "+g1.toString(10), descr: pHex});
+		$timeout(function(){
+			console.log("2");
+			$scope.lines.push({message: "Alice a2 = random exponent", descr: ""});
+			a2 = new BigInteger(1536,1,new SecureRandom());
+			var str = a2.toString(16);
+			$scope.lines.push({message: "Alice a2 = random "+ str.substring(0,10)+"...", descr: linebrk(str, 64)});
+			//$scope.$apply();
+		}, 10).then(function(){
+			console.log("3");
+			$scope.lines.push({message: "Alice a3 = random exponent", descr: ""});			
+			a3 = new BigInteger(1536,1,new SecureRandom());
+			str = a3.toString(16);
+			$scope.lines.push({message: "Alice a3 = random "+ str.substring(0,10)+"...", descr: linebrk(str, 64)});			
+			//$scope.$apply();
+		}).then(function(){
+			console.log("4");
+			$scope.lines.push({message: "Alice g2a = modPow", descr: ""});
+			/*g2a = g1.modPow(a2, p)
+			str = g2a.toString(16);
+			$scope.lines.push({message: "Alice g2a = "+ str.substring(0,10)+"...", descr: linebrk(str, 64)});*/
+			//$scope.lines.push({message: linebrk(hex2b64(nextRandom.toString(16)), 64)});
+		});
 	};
+	
     $scope.clear = function() {
 		var bi = new BigInteger(modpStr, 16);
 		$scope.lines.push({message:linebrk(bi.toString(16), 64)});
